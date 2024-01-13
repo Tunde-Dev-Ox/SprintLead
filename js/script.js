39eedafdffed19cfe9a22cb2beebfcc18126e5a7
Tunde <iamjosephtunde@gmail.com>
2024-01-13 04:31:16 +0100

    landing page completed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+const faqBtn = document.querySelectorAll('.faq-list-button');
+const faqAnswer = document.querySelectorAll('.faq-answer-wrapper');
+const faqIcon = document.querySelectorAll('.faq-svg');
+
+faqBtn.forEach((btn, index) => {
+    btn.addEventListener('click', (e) => {
+        faqAnswer.forEach((answer, i) => {
+            if (i !== index) {
+                answer.classList.remove('faq-is-active');
+            }
+        });
+        faqAnswer[index].classList.toggle('faq-is-active');
+        
+        faqIcon.forEach((icon, i) => {
+            if (i !== index) {
+                icon.classList.remove('rotate');
+            }
+        });
+        faqIcon[index].classList.toggle('rotate');
+    });
+});
+
