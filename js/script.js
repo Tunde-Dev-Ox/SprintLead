59cbf2cffb45e18bd085e1889f33b6439cc07791
Tunde <iamjosephtunde@gmail.com>
2024-01-15 18:38:16 +0100

    mission page

--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@
 const faqBtn = document.querySelectorAll('.faq-list-button');
 const faqAnswer = document.querySelectorAll('.faq-answer-wrapper');
 const faqIcon = document.querySelectorAll('.faq-svg');
+const menuBtn = document.querySelector('.hamburger-menuWrapper');
+const mobileMenu = document.querySelector('.nav-mobile-block');
+const body = document.querySelector('body');
 
+
+// FAQ implementation
 faqBtn.forEach((btn, index) => {
     btn.addEventListener('click', (e) => {
         faqAnswer.forEach((answer, i) => {
@@ -20,4 +25,13 @@ faqBtn.forEach((btn, index) => {
         faqIcon[index].classList.toggle('rotate');
     });
 });
+
+
+// Mobile menu implementation
+menuBtn.addEventListener('click', function () {
+    mobileMenu.classList.toggle('mobile-menu-active');
+    body.classList.toggle('mobile-menu-body');
+});
+
+
 
